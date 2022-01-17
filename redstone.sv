b8aba7110e3256e58d46a80b0fae10f6e95619a2
Ada Rosa <adrosa@microsoft.com>
2022-01-17 13:59:02 +0000

    working

--- redstone.sv
+++ redstone.sv
@@ -4,25 +4,18 @@
 // parameter WIDTH = 4;
 // parameter DEPTH = 2;
 //`include "define.v"
-`define WIDTH   12
+typedef bit [3:0] redstone;
 
-`define ALU_ADD 2'b00
-`define ALU_SUB 2'b01
-`define ALU_MAX 2'b10
-`define ALU_DIF 2'b11
-
-
-
-`define REDSTONE [3:0]
 
 module comparator_sub (
-  input `REDSTONE back, lside, rside,
-  output `REDSTONE front
+  input clk,
+  input redstone back, lside, rside,
+  output redstone front
 );
-  wire `REDSTONE side;
+  wire redstone side;
   max max_sides(lside, rside, side);
-  wire `REDSTONE side_db;
-  wire `REDSTONE back_db;
+  wire redstone side_db;
+  wire redstone back_db;
   
   debounce db_back(back, back_db);
   debounce db_side(side, side_db);
@@ -30,117 +23,56 @@ module comparator_sub (
   assign front = back_db - side_db;
 endmodule
 
-module max (input `REDSTONE a, 
-            input `REDSTONE b, 
-            output `REDSTONE out);
+module max (input redstone a, 
+            input redstone b, 
+            output redstone out);
   assign out = (a>b)? a : b;
 endmodule
 
-module min (input `REDSTONE a, 
-            input `REDSTONE b, 
-            output `REDSTONE out);
+module min (input redstone a, 
+            input redstone b, 
+            output redstone out);
   assign out = (a<b)? a : b;
 endmodule
 
-module debounce ( input `REDSTONE in, output `REDSTONE out
+module debounce ( input redstone in, output redstone out
 );
-  reg `REDSTONE old;
+  reg redstone old;
   set_dont_touch old
   buf #2 delay(old, in);
   min m(in, old, out); 
 endmodule
 
-
-
-// module full_oct_adder (
-//     input clk,
-//     input [2:0] a, b,
-// );
-    
-// endmodule
-
-// module serialalu
-//   (
-//     input wire reset,
-//     input wire [2:0] a, b,
-//     input wire [2:0] s,
-//     output reg out, carry
-//   );
-
-
-//   always @(reset, a, b, s)
-//   begin
-//       if (reset) begin
-//           carry = 0;
-//       end
-//     case (s)
-//       `ALU_ADD : // nop
-//         begin
-            
-//         end
-//         {cout, out} <= {c, a};
-      
-//     endcase
-//   end
-// endmodule
-
-// module alu
-//   #(parameter WIDTH = 4)
-//    (input wire [WIDTH-1:0] a, b,
-//     output [WIDTH-1:0] out,
-//     input wire [2:0] s,
-//     output cout
-//    );
-//   wire [WIDTH:0] c;
-//   assign c[0] = s[2];
-//   genvar i;
-//   generate
-//     for (i = 0; i < WIDTH; i=i+3)
-//     begin
-//       serialalu sa(a[i+WIDTH-1:i], b[i+WIDTH-1:i], out[i+WIDTH-1:i], c[i+WIDTH-1:i], s);
-//     end
-//   endgenerate
-//   assign cout = c[WIDTH];
-
-// endmodule
-
-// module fancy_torch (
-//     input clk, in,
-//     output out
-//   );
-//   reg [59:0] changes = 0;
-  
-//   reg [5:0] numchanges = 0;
-
-
-//   // shouldent be a reg
-//   wire oldest_change;
-//   wire[59:0] new_changes;
-//   wire burnout;
-//   wire changed;
-//   reg old_in = 0;
-  
-//   assign changed = old_in != in;
-//   assign {oldest_change, new_changes} = {changes, changed};
-//   assign burnout = numchanges >= 8;
-//   assign out = ~old_in & ~burnout;
-
-//   always @(posedge clk )
-//   begin
-//     changes <= new_changes;
-//     numchanges <= numchanges - oldest_change + changed;
-//     old_in <= in;
-//     //out <= ~in & ~burnout;
-//   end
-// endmodule
-
-
 module torch (
-    input in,
+    input clk, in,
     output out
   );
-  assign #1 out = in != 0;
+  reg [59:0] changes = 0;
+  
+  reg [5:0] numchanges = 0;
+
+
+  // shouldent be a reg
+  wire oldest_change;
+  wire[59:0] new_changes;
+  wire burnout;
+  wire changed;
+  reg old_in = 0;
+  
+  assign changed = old_in != in;
+  assign {oldest_change, new_changes} = {changes, changed};
+  assign burnout = numchanges >= 8;
+  assign out = ~old_in & ~burnout;
+
+  always @(posedge clk )
+  begin
+    changes <= new_changes;
+    numchanges <= numchanges - oldest_change + changed;
+    old_in <= in;
+    //out <= ~in & ~burnout;
+  end
 endmodule
+
 
 module repeater #(
   parameter delay = 4
@@ -148,7 +80,7 @@ module repeater #(
   input in, latch,
   output out
 );
-  logic state = latch? state: in; 
+  redstone state = latch? state: in; 
   buf #(1,delay,delay) b(out, state);
   
 endmodule
--- spaghetti.sv
+++ spaghetti.sv
@@ -3,17 +3,38 @@
 `define WORD_SIZE 8
 typedef bit [`WORD_SIZE-1:0] word;
 
-typedef enum bit [1:0]  { OP_ADD, OP_SUB, OP_XOR } alu_op;
+typedef enum bit [1:0]  { OP_ADD, OP_SUB, OP_XOR, OP_MOV } alu_op;
 
-typedef enum bit [1:0] { WB_NON, WB_ACC, WB_ANS, WB_REG  } writeback;
+typedef enum bit [1:0] { WB_NON, WB_ACC, WB_REG  } writeback_mode;
+
+
+typedef enum bit [2:0] {
+    V0, 
+    V1,
+    RA,
+    R1,
+    P1,
+    P2,
+    P3,
+    P4  } register_name;
+
 
 typedef struct packed {
-    bit exec_if_flag;
-    bit exec_if_not_flag;
-    bit exe_flag_set;
-    alu_op op;
-    writeback wb_reg;
-    writeback wb_acc;
+    bit clk;
+    word data;
+} io_bus;
+
+typedef struct packed {
+    bit if_flag;
+    bit if_not_flag;
+    bit set_flag;
+} exec_opts;
+
+typedef struct packed {
+    exec_opts exec; //3
+    alu_op op; // 2
+    register_name r; //3
+    register_name write_to; //3
 } instruction;
 
 module rom (
@@ -30,23 +51,53 @@ module rom (
 endmodule
 
 
+module register (
+    input word data_in,
+    input bit write, read,
+    output bit read_ready, write_done,
+    output word data_out,
+);
+
+    word data;// = write ? data_in : data;
+    assign data_out = read ? data_out : 0;
+    assign read_ready = read;
+    assign write_done = write;
+    always @(posedge write ) begin
+        data = data_in;
+    end
+endmodule
+
 module port (
     input clk,
-    input word a_data_in, b_data_in,
-    input bit a_write, a_read, b_write, b_read,
-    output bit read_start, write_finish,
-    output word a_data_out, b_data_out,
+    input word a_data_in,
+    input bit a_write, a_read,
+    output bit a_read_ready, a_write_done,
+    output word a_data_out,
+
+    input word a_data_in,
+    input bit a_write, read,
+    output bit a_read_ready, a_write_done,
+    output word a_data_out,
 );
+    register a(
+        .data_in(a_data_in),
+        .write(a_write),
+        .read(b_read),
+        .read_ready(b_read_ready),
+    );
+    register b();
     assign a_data_out= a_data_in;
     assign b_data_out = b_data_in;
 endmodule
 
 module alu (
     //input bit clk,
+    input start,
     input word a, b,
     input alu_op op,
     output word o,
-    output bit flag
+    output bit flag,
+    output outputing,
 );
     //reg [`WORD_SIZE:0] result;
     //reg carry;
@@ -55,7 +106,9 @@ module alu (
         op == OP_ADD ? a + b :
         op == OP_SUB ? a - b :
         op == OP_XOR ? {a == b, a ^ b} :
+        op == OP_MOV ? { b != 0 , b} :
         0;
+    assign finished = start;
     //op == OP_ADD 
     //1'b1: result <= a + b;
             //o = a+b;
@@ -70,19 +123,103 @@ module cpu (
     input clk
 );
     reg flag;
+
+
+always @(posedge clk ) begin
+    
+end
+
     reg step = clk;
 
     bit read_ready;
     bit stall = !read_ready;
-    word acc;
+    
+    register acc(
+        .data_in(result),
+        .read(b_ready),
+        .write(write_to_acc),
+        .read_ready(alu_start),
+        .write_done(write_done_a),
+        .data_out(a_value)
+    );
+
+    // input word data_in,
+    // input bit write, read,
+    // output bit read_ready, write_done,
+    // output word data_out,
+
+
+    register r1(
+        .data_in(result),
+        );
+
+    word a_value;
+    
+    word b_value;
+
+    word result;
+    
+    bit flag_output;
+
+    bit writeback;
+    
+    bit writeback_done = write_done_a | write_done_r1;
+
+    bit write_done_a;
+    bit write_done_r1;
+
+    word write_to_acc = current_instruction.write_to == WB_ACC && writeback;
+    word write_to_reg = current_instruction.write_to == WB_REG && writeback;
+
+    bit alu_start;
+    alu alu(
+        .start(alu_start),
+        .a(a_value), 
+        .b(b_value),
+        .op(current_instruction.op),
+        .o(result),
+        .flag(flag_output),
+        .outputing(writeback));
+
+
+
+    word write_out;
+
+
+
+
+
     rom rom(.clk(step), .out(current_instruction));
     instruction last_instruction;
     instruction current_instruction;
     always @(posedge clk ) begin
         last_instruction = current_instruction;
-        case (current_instruction.wb_reg)
-             : 
-            default: 
-        endcase
+        // case (current_instruction.wb_reg)
+        //      : 
+        //     default: 
+        // endcase
     end
+endmodule
+
+// this is not making the code cleaer
+module register_file (
+    input register_name reg_write, reg_read,
+    input word data_in,
+    input bit write, read,
+    output bit read_ready, write_done,
+    output word data_out
+);
+
+    bit [7: 0] do_write = write < reg_write;
+    bit [7:0] do_read = (read < reg_read1) | (read < reg_read2);
+    assign data_out = read ? (
+        reg_read == V0 ? 0 :
+        reg_read == V1 ? 1 :
+        reg_read == VI ? :
+        reg_read == R1 ? :
+        reg_read == P1 ? :
+        reg_read == P2 ? :
+        reg_read == P3 ? :
+        reg_read == P4 ? : 0
+    )  : 0;
 endmodule
